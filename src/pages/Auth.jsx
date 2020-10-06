97ba47e557105e885385c755d2f84a35c037e6d0
Alina <ka-front@fulogy.com>
2020-10-06 11:59:16 +0300

    auth form styles

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -16,22 +16,71 @@ const Auth = (props) => {
         props.sendRequest(name, password);
     };
     return (
-        <Container>
-            <form onSubmit={handleSubmit}>
-                <Input placeholder="name" onChange={handleSetName} />
-                <Input placeholder="password" onChange={handleSetPassword} />
-                <Button type="submit">GO!</Button>
-            </form>
-        </Container>
+        <Wrapper>
+            <Container>
+                <Title>Log in to see The Sweetest List Ever</Title>
+                <Form onSubmit={handleSubmit}>
+                    <Input placeholder="name" onChange={handleSetName} />
+                    <Input
+                        placeholder="password"
+                        onChange={handleSetPassword}
+                    />
+                    <Button type="submit">GO!</Button>
+                </Form>
+            </Container>
+        </Wrapper>
     );
 };
 
-const Container = styled.div`
-    height: fit-content;
-    width: fit-content;
+const Wrapper = styled.div`
+    display: grid;
+    height: 100vh;
+    place-items: center;
 `;
-const Input = styled.input``;
+const Container = styled.div``;
+const Title = styled.h2`
+    color: #f14487;
+    font-size: 32px;
+    font-weight: 400;
+    margin-bottom: 30px;
+`;
+const Form = styled.form``;
+const Input = styled.input`
+    border: 1px solid #f14487;
+    box-shadow: 0px 4px 4px rgba(241, 68, 135, 0.4),
+        0px 4px 4px rgba(241, 68, 135, 0.4), 0px 4px 4px rgba(241, 68, 135, 0.4),
+        0px 4px 4px rgba(241, 68, 135, 0.4);
+    border-radius: 7px;
+    display: block;
+    height: 30px;
+    margin-bottom: 30px;
+    padding-left: 10px;
+    width: 300px;
+    &:focus {
+        outline: 0;
+    }
+`;
 
-const Button = styled.button``;
+const Button = styled.button`
+    background-color: #f14487;
+    border-radius: 7px;
+    border: none;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    color: #fff;
+    cursor: pointer;
+    height: 30px;
+    outline: none:
+    transition: box-shadow 0.2s ease, background-color 0.2s ease;
+    width: 80px;
+    &:hover {
+        background-color: #cb3a72;
+        box-shadow: 0 2px 2px rgba(0, 0, 0, 0.25);
+    }
+    &:active {
+        background-color: #a1335e;
+        box-shadow: 0 0 0 rgba(0, 0, 0, 0.25);
+    }
+    &:focus {outline:0;}
+`;
 
 export default Auth;
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -2,7 +2,7 @@ export const setToken = (token) => {
     localStorage.setItem("token", token);
 };
 
-export const removeToken = (token) => {
+export const removeToken = () => {
     localStorage.removeItem("token");
 };
 
